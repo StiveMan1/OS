6f82825455cd2edcb5810c9e40863ea6c87c9ee4
StiveMan1 <04024004zh@gmail.com>
2021-10-03 19:19:37 +0300

    week7

---
+++ week7/ex2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main() {
+    int n;
+    printf("Write size of the array:\n");
+    scanf("%d", &n);
+    int* a = malloc(n*sizeof(int));
+    for(int i = 0 ; i < n ; i++){
+        a[i] = i + 1;
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    if(a != NULL){
+	    free(a);
+    }
+    return 0;
+}
